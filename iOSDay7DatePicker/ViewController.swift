f91d601f3b0b4ea2bcc59fbab459adf21e7e8257
philip <jhasondeleon@yahoo.com>
2020-03-09 21:01:31 -0400

    commit

--- iOSDay7DatePicker/ViewController.swift
+++ iOSDay7DatePicker/ViewController.swift
@@ -10,11 +10,59 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textField_Date: UITextField!
+    var datePicker : UIDatePicker!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        //textField_Date.delegate = self
+    }
+    
+    override func didReceiveMemoryWarning()
+    {
+        super.didReceiveMemoryWarning()
+    }
+    
+    func textFieldDidBeginEditing(_ textField: UITextField)
+    {
+        self.pickUpdate(self.textField_Date)
+    }
+    
+    func pickUpdate(_ textField : UITextField)
+    {
+        self.datePicker = UIDatePicker(frame: CGRect(x: 0, y: 0, width: self.view.frame.size.width, height: 216))
+        self.datePicker.backgroundColor = UIColor.white
+        self.datePicker.datePickerMode = UIDatePicker.Mode.date
+        textField.inputView = self.datePicker
+        
+        let toolBar = UIToolbar()
+        toolBar.barStyle = .default
+        toolBar.isTranslucent = true
+        toolBar.tintColor = .red//UIColor(red: 92/244, green: 216/255, blue: 255/255, alpha: 1)
+        toolBar.sizeToFit()
+        
+        let doneButton = UIBarButtonItem(title: "Done", style: .plain, target:  self, action: #selector(ViewController.doneClick))
+        let spaceButton = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        let cancelButton = UIBarButtonItem(title: "Cancel", style: .plain, target: self, action: #selector(ViewController.cancelClick))
+        
+        toolBar.setItems([cancelButton, doneButton], animated: false)
+        toolBar.isUserInteractionEnabled = true
+        textField.inputAccessoryView = toolBar
     }
 
+    @objc func doneClick() {
+        let dateFormatter1 = DateFormatter()
+        dateFormatter1.dateStyle = .medium
+        dateFormatter1.timeStyle = .none
+        textField_Date.text = dateFormatter1.string(from: datePicker.date)
+        textField_Date.resignFirstResponder()
+    }
+    
+    @objc func cancelClick()
+    {
+        textField_Date.resignFirstResponder()
+    }
 
 }
 
